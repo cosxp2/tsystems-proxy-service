5742dc2aa62632d87d1113a3b253f8271d9125de
Cosmin Pricop <cosminxpricop@gmail.com>
2024-10-18 02:54:29 +0300

    Validate AWS credentials

--- app/config.py
+++ app/config.py
@@ -7,3 +7,7 @@ class Config:
     AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
     AWS_SECRET_ACCESS = os.getenv("AWS_SECRET_ACCESS")
     AWS_REGION = os.getenv("AWS_REGION")
+    
+    if not AWS_ACCESS_KEY or not AWS_SECRET_ACCESS or not AWS_REGION:
+        raise EnvironmentError("One or more required environment variables are missing: "
+                               "AWS_ACCESS_KEY, AWS_SECRET_ACCESS, AWS_REGION")
