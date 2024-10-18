8c38e00e004e0aac062c4c46bfa6d75814095ae3
Cosmin Pricop <cosminxpricop@gmail.com>
2024-10-18 12:24:58 +0300

    Validate input file size and type

--- app/main.py
+++ app/main.py
@@ -6,8 +6,24 @@ from app.s3_manager import S3Manager
 app = FastAPI()
 s3_manager = S3Manager()
 
+ALLOWED_MAX_SIZE = 10 * 1024 * 1024
+ALLOWED_TYPES = ["text/plain"]
+
+# Not a requirement - just to showcase that file validation should be performed
+def validate_file(file: UploadFile = File(...)):
+    data = file.file.read()
+    
+    if len(data) > ALLOWED_MAX_SIZE:
+        raise HTTPException(status_code=400, detail="File exceeds the maximum allowed size")
+    if file.content_type not in ALLOWED_TYPES:
+        raise HTTPException(status_code=400, detail="Unsupported file format")
+
+    # Point back to file beginning
+    file.file.seek(0)
+
 @app.post("/upload/")
 async def upload_file(bucket_name: str, file_name: str, file: UploadFile = File(...)):
+    validate_file(file)
     res = s3_manager.upload_file(file.file, file_name, bucket_name)
     if "error" in res:
         raise HTTPException(status_code=400, detail=res["error"])
